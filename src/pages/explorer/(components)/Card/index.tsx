14cff629fc14db7e36c1544064f8f40f087f562f
Pedro Cruz <santahelia.pedro@gmail.com>
2023-07-04 21:31:37 -0300

    add images

--- src/pages/explorer/(components)/Card/index.tsx
+++ src/pages/explorer/(components)/Card/index.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import ImageBook from '../../../../../public/prov/Book.svg';
 import { Card } from './styles';
 import { Star } from 'phosphor-react';
 
@@ -13,7 +12,7 @@ interface interfaceBook {
 export default function CardExplorer({ name, author, cover_url }: interfaceBook) {
     return(
         <Card>
-            <Image src={ImageBook} alt={name} />
+            <Image src={cover_url} alt={name} />
             <section>
                 <header>
                     <h2>{ name }</h2>
